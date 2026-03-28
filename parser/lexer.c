c42e15aea41551f489472a873d1f7e951f93fe06
Aleyna Say <asay@k1m22s05.42istanbul.com.tr>
2026-03-28 19:11:24 +0300

    init: lexer

---
+++ parser/lexer.c
@@ -0,0 +1,35 @@
+#include "minishell.h"
+
+int	nonspace_index(char *str, int ind)
+{
+	while(str[ind] != ' ' && str[ind] != '\t')
+		ind++;
+	return ind;
+}
+
+void	get_value(char *str, int i)
+{
+	int j;
+	t_token *token;
+
+	//mem allocate for token
+	j = nonspace_index(str, i);
+	while (i < j)
+	{
+		if (str[i] == '>')
+		{
+			if(str[i+1] == '>')
+				token->type = APPEND;
+			else
+				token->type = REDIRECT_OUT;
+		}
+		if (str[i] == '<')
+		{
+			if(str[i+1] == '<')
+				token->type = HEREDOC;
+			else
+				token->type = REDIRECT_IN;
+		}
+		i++;
+	}
+}
