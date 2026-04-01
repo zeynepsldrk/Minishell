a50284becaf61cea27dab83bf59fd2391d0c97d4
Aleyna Say <asay@k1m18s02.42istanbul.com.tr>
2026-04-01 19:09:12 +0300

    feat: add token extraction (reafctor needed)

--- parser/lexer.c
+++ parser/lexer.c
@@ -1,48 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asay <asay@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/31 19:37:52 by asay              #+#    #+#             */
+/*   Updated: 2026/04/01 19:04:22 by asay             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int	nonspace_index(char *str, int ind)
+int lexer(t_shell *shell, char *str)
 {
-	while(str[ind] != ' ' && str[ind] != '\t')
-		ind++;
-	return ind;
+    t_token *tokens;
+
+    tokens = get_tokens(str);
+    if(!tokens)
+        return (0);
+    //tokenleri sırayla yazdırıp kontrol et.
+    return (1);
 }
 
-t_token	*new_token(t_token_type type, char *value)
+t_token *get_tokens(char *str)
 {
-	t_token	*token;
+    int i;
+    int j;
+    char *buff;
+    t_token *head;
+    t_token *curr;
+    t_token_type value;
 
-	token = malloc(sizeof(t_token));
-	if(!token)
-		return (NULL);
-	token->type = type;
-	token->value = value;
-	token->ne = NULL;
-	return (token);
+    i = 0;
+    head = NULL;
+    buff = malloc(sizeof(char) * (ft_strlen(str) + 1));
+    if(!buff)
+        return NULL;
+    
+    //get_value öncesi "$USER" -> asay : '$USER' -> $USER durumu kontrol edilmeli.
+    while(str[i])
+    {
+        value = get_value(str, i);
+        if(str[i] == 32)
+        {
+            if(!buff[0])
+                i++;
+            else
+            {
+                curr = new_token(value, buff);
+                add_token(&head, curr);
+                buff[0] = '\0';
+                i++;
+            }
+        }
+        else if(str[i] == '|' || str[i] == '<' || str[i] == '>')
+        {
+            if(!buff[0])
+                i++;
+            /* |, >, < durumu: new_token(type, value) çağırman lazım. 
+                Tip belirleme mantığı da burada olacak — str[i+1]'e bakarak 
+                PIPE, REDIRECT_OUT, APPEND vs. kararını veriyorsun.
+            */
+            else
+            {    
+                curr = new_token(value, buff);
+                if(str)
+                add_token(&head, curr);
+                buff[0] = '\0';
+                i++;
+            }
+        }
+        else if(str[i] == 34 || str[i] == 39)
+        {
+            /*
+            Her token tamamlandığında — boşluk, |/>/<, veya tırnak kapandığında — şu üç şeyi yapman lazım:
+
+                - new_token ile token oluştur
+                - add_token ile listeye ekle
+                - buff[0] = '\0' ile buffer'ı sıfırla
+
+            Normal karakter gördüğünde tek şey: buff[j++] = str[i] — karakteri buffer'a ekle, j'yi ilerlet.
+            i her durumda ilerlemeli — bunu unutma, sonsuz döngü riski var.
+            */
+            if(!buff[0])
+                i++;
+            {    
+                curr = new_token(value, buff);
+                if(str)
+                add_token(&head, curr);
+                buff[0] = '\0';
+                i++;
+            }
+        }
+        else
+            buff[j++] = str[i++];
+    }
+    return (head);
 }
 
-void	get_value(char *str, int i)
-{
-	int j;
-	t_token *token;
-
-	//mem allocate for token
-	j = nonspace_index(str, i);
-	while (i < j)
-	{
-		if (str[i] == '>')
-		{
-			if(str[i+1] == '>')
-				token->type = APPEND;
-			else
-				token->type = REDIRECT_OUT;
-		}
-		if (str[i] == '<')
-		{
-			if(str[i+1] == '<')
-				token->type = HEREDOC;
-			else
-				token->type = REDIRECT_IN;
-		}
-		i++;
-	}
-}
