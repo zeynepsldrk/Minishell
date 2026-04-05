8febde08b651dd687f84cf5e4247b2fb9f5536f2
Zeynep Sıla Durak <durakzeynepsila@gmail.com>
2026-04-05 15:43:18 +0300

    added env struct

--- include/minishell.h
+++ include/minishell.h
@@ -4,8 +4,8 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+//# include <readline/readline.h>
+//# include <readline/history.h>
 
 typedef enum e_token_type
 {
@@ -38,11 +38,21 @@ typedef struct s_cmd
 	t_redirect *redirects;
 } t_cmd;
 
+typedef struct s_env_node
+{
+    char *key;
+    char *value;
+    int has_value;
+    struct s_env_node *next;
+} t_env_node;
+
 typedef struct s_minishell
 {
+    t_env_node *env_list;
 	char **env;
 	int exit_value;
 	t_token *tokens;
 } t_shell;
+
 
 #endif
