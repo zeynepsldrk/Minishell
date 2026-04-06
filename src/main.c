ff9b0c32e62ec67d61e49f4c271eb3dea783da65
Zeynep Sıla Durak <durakzeynepsila@gmail.com>
2026-04-06 18:43:19 +0300

    feat: add minishell main loop, shell initialization and command dispatch skeleton

--- src/main.c
+++ src/main.c
@@ -1,7 +1,27 @@
 #include "minishell.h"
 
+t_cmd *start_parser(char *input, t_shell *shell)
+{
+    (void)input;
+    (void)shell;
+    return (NULL);
+}
+
 void start_execute(t_shell *shell)
 {
+    t_cmd *cmd;
+
+    cmd = shell->cmds;
+    if (!cmd || !cmd->argv)
+        return ;
+    /*Burada yapmaya çalıştığım pipe kullanımına göre ayırmak ve kullanılmadığı takdirde builtin ya da external
+    olarak çalıştırılması, ek olarak içeride redirect işlemleri kontrolü de yapılmalıdır.*/
+    if (cmd->next != NULL) //birden fazla cmd varsa pipe var demektir
+        pipe_working(shell); //cmd listesini dolaşarak pipe işlemlerini yapar
+    else if (is_builtin(cmd->argv[0])) //tek cmd varsa ve bu cmd bir built-in komutsa
+        execute_builtin(shell); //built-in komutları çalıştırır. İçeride redirect işlemleri kontrolü yapmayı unutma
+    else
+        execute_external(shell); //external komutları çalıştırır. İçeride redirect işlemleri kontrolü yapmayı unutma
     
 }
 
@@ -9,14 +29,14 @@ void lets_start_shell(t_shell *shell)
 {
     while (1)
     {
-        shell->input = readline("minishell");
+        shell->input = readline("minishell> ");
         if(shell->input == NULL) //crtl+d de readline NULL döner
         {
-            write(2, "\nexit\n", 6);
+            write(2, "exit\n", 5);
             rl_clear_history();
             break ;
         }
-        if (shell->input != '\0') //bash de enter a basınca boş inputu kaydetmiyor soo biz de kaydetmiyoruz
+        if (*shell->input != '\0') //bash de enter a basınca boş inputu kaydetmiyor soo biz de kaydetmiyoruz
             add_history(shell->input);
         shell->cmds = start_parser(shell->input, shell); 
         if (shell->cmds)
@@ -37,20 +57,20 @@ t_shell *init_shell(t_shell *shell, char **envp)
 
 int main(int argc, char **argv, char **envp)
 {
-    t_shell *shell; //zurnanın zırt dediği yerdirr
+    t_shell *shell;
     (void)argc;
     (void)argv;
 
     shell = malloc(sizeof(t_shell));
     if (!shell)
-        return (NULL); 
-    if (!init_shell(shell, envp))
+        return (1); 
+    if (!init_shell(shell, envp)) //default değerlerle dolduruldu
     {
         write(2, "Initialization failed\n", 22);
         free(shell);
-        return (NULL);
+        return (1);
     }
-    lets_start_shell(shell);
+    lets_start_shell(shell); //repl döngüsü başlatıldı
     free(shell);
     return (0);
 }
