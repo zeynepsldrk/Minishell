c0175634dae7aee8163912cb26960d5e8a54832a
Zeynep Sıla Durak <durakzeynepsila@gmail.com>
2026-04-06 18:45:02 +0300

    fix: declared readline libary

--- include/minishell.h
+++ include/minishell.h
@@ -4,8 +4,8 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-//# include <readline/readline.h>
-//# include <readline/history.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 typedef enum e_token_type
 {
