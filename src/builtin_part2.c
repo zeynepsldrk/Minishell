3a3a624fa8686a3a7b42f9951925c12001595f0f
Zeynep Sıla Durak <durakzeynepsila@gmail.com>
2026-04-08 10:29:43 +0300

    moved to a specific folder

--- src/builtin_part2.c
+++
@@ -1,7 +0,0 @@
-#include "minishell.h"
-
-int builtin_exit(t_shell *shell);
-int builtin_pwd(t_shell *shell);
-int builtin_export(t_shell *shell);
-int builtin_unset(t_shell *shell);
-int builtin_env(t_shell *shell);
