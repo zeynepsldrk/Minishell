ceed520786fed2f2606a406064ede2b69d9d9d25
Zeynep Sıla Durak <durakzeynepsila@gmail.com>
2026-04-16 00:12:30 +0300

    added new func

--- src/utills.c
+++ src/utills.c
@@ -1,5 +1,25 @@
 #include "minishell.h"
 
+int redir_error(int fd)
+{
+    if (fd < 0)
+    {
+        perror("open");
+        return (1);
+    }
+    return (0);
+}
+
+int str_len(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
 int	str_cmp(char *s1, char *s2)
 {
 	int	i;
