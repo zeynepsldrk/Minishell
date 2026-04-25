f652b4bdce35adafa6fc4c7fda542aaa3e5c503d
Aleyna Say <sayaleyna27@gmail.com>
2026-04-25 12:28:08 +0300

    add new functions for cleaning cmds and tkns

--- parser/clean.c
+++ parser/clean.c
@@ -25,10 +25,47 @@ void    free_str(char **str)
     free(str);
 }
 
+void free_tokens(t_token *token)
+{
+    t_token *ptr;
+
+    while(token)
+    {
+        ptr = token;
+        token = token->next;
+        if(ptr->context)
+            free(ptr->context);
+        free(ptr);
+    }
+    ptr = NULL;
+}
+
+void free_commands(t_cmd *cmds)
+{
+    t_cmd *tmp;
+
+    while (cmds)
+    {
+        tmp = cmds;
+        cmds = cmds->next;
+        if (tmp->argv)
+            free_str(tmp->argv); // char **argv temizliği
+        //if (tmp->redirects)
+            //free_redirects(tmp->redirects); // t_redirect listesi temizliği
+        free(tmp);
+    }
+}
+
+void clean_get_tkns(t_lexer *ptr)
+{
+    free(ptr->buff);
+    free(ptr);
+}
+
 void free_sh(t_shell *shell)
 {
     free_str(shell->env);
-    //free_tokens(shell->tokens);
-    //free_tokens(shell->cmds);
+    free_tokens(shell->tokens);
+    free_commands(shell->cmds);
     free(shell);
 }
