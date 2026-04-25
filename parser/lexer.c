290bc49554cffb7536c508b3affa763b062bd748
Aleyna Say <asay@k1m04s05.42istanbul.com.tr>
2026-04-25 19:22:47 +0300

    reorganize function locations

--- parser/lexer.c
+++ parser/lexer.c
@@ -6,7 +6,7 @@
 /*   By: asay <asay@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 19:37:52 by asay              #+#    #+#             */
-/*   Updated: 2026/04/02 22:30:39 by asay             ###   ########.fr       */
+/*   Updated: 2026/04/25 17:18:29 by asay             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,95 +31,54 @@ int lexer(t_shell *shell, char *str)
     return (1);
 }
 
-// boşluk görüldüğünde bufferdaki veriyi listeye ekleyip buffer'ı boşaltır. sonra bir sonraki anlamlı ifadeye kadar boşlukları atlar.
-void whitespace_tkn(t_lexer *ptr, char *str) 
+t_token	*new_token(t_token_type type, char *context)
 {
-    if(ptr->buff[0] || ptr->has_quote)
-    {
-        ptr->curr = new_token(WORD, ptr->buff);
-        add_token(&ptr->head, ptr->curr);
-        ft_memset(ptr->buff, 0, ptr->j);
-        ptr->j = 0;
-        ptr->has_quote = 0; 
-    }
-    while(str[ptr->i] == 32 && str[ptr->i])
-        ptr->i++;
+	t_token	*token;
+
+	token = malloc(sizeof(t_token));
+	if(!token)
+		return (NULL);
+	token->type = type;
+	token->context = ft_strdup(context);
+	token->next = NULL;
+	return (token);
 }
 
-void redirect_tkn(t_lexer *ptr, char *str)
+//kontrol edildi.
+//gelen inputta str[*i]'nin ne olduğunu kontrol ediyoruz.
+t_token_type	get_value(char *str, int *i) 
 {
-    // eğer bufferde herhangi bir şey varsa ve boşluksuz yazılmışsa whitespace_tkn çağırılmaz.
-    // bu yüzden whitesapce_tkn'nin ekleyemeyeceği WORD token'ları için bir kontrol ekledik.
-    if(ptr->buff[0] || ptr->has_quote)
-    {
-        ptr->curr = new_token(WORD, ptr->buff);
-        add_token(&ptr->head, ptr->curr);
-        ft_memset(ptr->buff, 0, ptr->j);
-        ptr->j = 0;
-        ptr->has_quote = 0;
-    }
-    //redirect > görüldüğü her koşulda eklenecek o yüzden herhangi bir if yok
-    if(ptr->value == APPEND)
-        ptr->curr = new_token(ptr->value, ">>");
-    else if(ptr->value == HEREDOC)
-        ptr->curr = new_token(ptr->value, "<<");
-    else if(ptr->value == REDIRECT_OUT)
-        ptr->curr = new_token(ptr->value, ">");
-    else
-        ptr->curr = new_token(ptr->value, "<");
-    add_token(&ptr->head, ptr->curr);
-    if(ptr->value == APPEND || ptr->value == HEREDOC)
-        ptr->i++;
-    ptr->i++;
-    while(str[ptr->i] == 32)
-        ptr->i++;
-    ft_memset(ptr->buff, 0, ptr->j);
-    ptr->j = 0;
+	if (str[*i] == '>')
+	{
+		if(str[*i+1] == '>')
+			return(APPEND);
+		return(REDIRECT_OUT);
+	}
+	else if (str[*i] == '<')
+	{
+		if(str[*i+1] == '<')
+			return(HEREDOC);
+		return(REDIRECT_IN);
+	}
+	else if(str[*i] == '|')
+		return(PIPE);
+	else
+		return(WORD);
 }
 
-// çift tırnak veya tek tırnak görünce çağrılır ve tırnak karakterini atlayarak başlar, aynı tırnak karakteriyle karşılaşana kadar buffera yazmaya devam eder.
-void quote_tkn(t_lexer *ptr, char *str)
+void add_token(t_token **head, t_token *new)
 {
-    char quote_type;
-    ptr->has_quote = 1; //' veya " gorgugumuzde has_quote'u 1 yapıyoruz ki kapanmamış tırnaklar haric her durumda (bos girdiler icin daha cok) get_tokens'taki if'e girilsin.
-    //int temp;
+	t_token *temp;
 
-    //temp = ptr->j;
-    quote_type = str[ptr->i];
-    ptr->i++;
-    while(str[ptr->i] && str[ptr->i] != quote_type) //bir sonraki tırnaga kadar buffer dolduruyoruz.
-        ptr->buff[ptr->j++] = str[ptr->i++];
-    if (str[ptr->i] == quote_type) // tirnak kapandi 
-        ptr->i++; // quote karakterini atla
-    else
-        printf("Error: Open quote!\n"); // quote kapanmadan fonksiyon bittiyse hata verdiriyoruz.
-    //burada has_quote = 1 yapildigi icin get_tokens'a geri dondugumuzde sondaki if sayesidne "kullanici tirnak kullandi, bir WORD token'i eklenmeli" diyebilmeyi sagliyoruz
-}
-// "hello"aleyna"world" -> "helloaleynaworld" şeklinde tek bir token olarak alınacak. 
-// yukarıdaki farklı kombinasyonlara göre fonksiyon geliştirilecek.
-// bash nasıl çalışıyor inceleyip ona göre davranırız.
-
-void pipe_tkn(t_lexer *ptr, char *str)
-{
-    //yapılan işlemler redirect_tkn ile aynı. çağırıldıkları yerde karmaşa olmaması için şimdilik ayırdım.
-    // eğer bufferde herhangi bir şey varsa ve boşluksuz yazılmışsa whitespace_tkn çağırılmaz.
-    // bu yüzden whitesapce_tkn'nin ekleyemeyeceği WORD token'ları için bir kontrol ekledik.
-    if(ptr->buff[0] || ptr->has_quote)
-    {
-        ptr->curr = new_token(WORD, ptr->buff);
-        add_token(&ptr->head, ptr->curr);
-        ft_memset(ptr->buff, 0, ptr->j);
-        ptr->j = 0;
-        ptr->has_quote = 0;
-    }
-    //pipe | görüldüğü her koşulda eklenecek o yüzden herhangi bir if yok
-    ptr->curr = new_token(ptr->value, "|"); //zaten get_value'dan value burada saklanıyor. context'in | olduğu belli. direkt boş string veriyoruz. 
-    add_token(&ptr->head, ptr->curr);
-    ptr->i++;
-    while(str[ptr->i] == 32)
-        ptr->i++;
-    ft_memset(ptr->buff, 0, ptr->j);
-    ptr->j = 0;
+	if(!*head)
+	{
+		*head = new;
+		return ;
+	}
+	temp = *head;
+	while(temp->next)
+    	temp = temp->next;
+	temp->next = new;
 }
 
 t_token *get_tokens(char *str)
@@ -150,4 +109,3 @@ t_token *get_tokens(char *str)
     clean_get_tkns(ptr);
     return (copy_of_head);
 }
-
