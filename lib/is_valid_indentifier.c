f69d7001fa2559de29bc6790da0c45a01878256d
Zeynep Sila Durak <zedurak@k1m05s09.42istanbul.com.tr>
2026-04-29 22:09:01 +0300

    added header

--- lib/is_valid_indentifier.c
+++ lib/is_valid_indentifier.c
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_valid_indentifier.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zedurak <zedurak@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/29 18:26:15 by zedurak           #+#    #+#             */
+/*   Updated: 2026/04/29 18:26:57 by zedurak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int is_valid_identifier(char *arg)
+int	is_valid_identifier(char *arg)
 {
-    int j;
+	int j;
 
-    j = 0;
-    if (!(ft_isalpha(arg[0]) || arg[0] == '_'))
-        return (1);
-    while (arg[j] && arg[j] != '=')
-    {
-        if (arg[j] == '+' && arg[j + 1] == '=')
-            return (0);
-        if (!(ft_isalnum(arg[j]) || arg[j] == '_'))
-            return (1);
-        j++;
-    }
-    return (0);
+	j = 0;
+	if (!(ft_isalpha(arg[0]) || arg[0] == '_'))
+		return (1);
+	while (arg[j] && arg[j] != '=')
+	{
+		if (arg[j] == '+' && arg[j + 1] == '=')
+			return (0);
+		if (!(ft_isalnum(arg[j]) || arg[j] == '_'))
+			return (1);
+		j++;
+	}
+	return (0);
 }
