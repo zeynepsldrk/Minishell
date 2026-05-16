f6eb6dfddf6b6f01daefc89ae09076be7d374a3c
Zeynep Sila Durak <zedurak@k1m18s02.42istanbul.com.tr>
2026-05-16 14:12:51 +0300

    added signal function

--- src/exc_external.c
+++ src/exc_external.c
@@ -6,7 +6,7 @@
 /*   By: zedurak <zedurak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 18:48:36 by zedurak           #+#    #+#             */
-/*   Updated: 2026/04/29 22:10:10 by zedurak          ###   ########.fr       */
+/*   Updated: 2026/05/16 13:56:10 by zedurak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,46 +17,46 @@ X_OK: Çalıştırma (execute) yetkin var mı?*/
 
 char *ft_join_and_free(char *s1, char *s2)
 {
-    char *res = ft_strjoin(s1, s2);
-    free(s1);
-    return (res);
+	char *res = ft_strjoin(s1, s2);
+	free(s1);
+	return (res);
 }
 
 void ft_free_split(char **arr)
 {
-    int i = 0;
-    while (arr[i])
-        free(arr[i++]);
-    free(arr);
+	int i = 0;
+	while (arr[i])
+		free(arr[i++]);
+	free(arr);
 }
 
 char *check_the_path(char *path, char *cmd)
 {
-    char **splited_path;
-    char *full_path;
-    int  i;
+	char **splited_path;
+	char *full_path;
+	int  i;
 
-    splited_path = ft_split(path, ':');
-    i = 0;
-    while (splited_path[i])
-    {
-        full_path = ft_strjoin(splited_path[i], "/");
-        full_path = ft_join_and_free(full_path, cmd);
-        if (access(full_path, F_OK) == 0)
-        {
-            ft_free_split(splited_path);
-            return (full_path);
-        }
-        free(full_path);
-        i++;
-    }
-    ft_free_split(splited_path);
-    return (NULL);
+	splited_path = ft_split(path, ':');
+	i = 0;
+	while (splited_path[i])
+	{
+		full_path = ft_strjoin(splited_path[i], "/");
+		full_path = ft_join_and_free(full_path, cmd);
+		if (access(full_path, F_OK) == 0)
+		{
+			ft_free_split(splited_path);
+			return (full_path);
+		}
+		free(full_path);
+		i++;
+	}
+	ft_free_split(splited_path);
+	return (NULL);
 }
 
 void	print_path_error(char *cmd, char *msg, int exit_code)
 {
-    write(2, "minishell: ", 11);
+	write(2, "minishell: ", 11);
 	if (cmd && *cmd)
 	{
 		write(2, cmd, ft_strlen(cmd));
@@ -70,14 +70,14 @@ void	print_path_error(char *cmd, char *msg, int exit_code)
 
 int	is_path_okey(char *path)
 {
-    struct stat st;
-    
+	struct stat st;
+	
 	if (access(path, F_OK)) //dosya disk de var mı?
 	{
 		print_path_error(path, "command not found", 127);
 		return (1);
 	}
-    stat(path, &st);
+	stat(path, &st);
 	if (S_ISDIR(st.st_mode)) // Klasör mü, dosya mı?
 	{
 		print_path_error(path, "is a directory", 126);
@@ -94,7 +94,7 @@ int	is_path_okey(char *path)
 int	is_absolute_path(char *path)
 {
 	if (!path || !path[0])
-        return (1);
+		return (1);
 	if (path[0] == '/')
 		return (0);
 	if (path[0] == '.')
@@ -113,16 +113,25 @@ char	*find_command_path(t_shell *shell)
 	{
 		if (is_path_okey(shell->cmds->argv[0]) == 0)
 			return (ft_strdup(shell->cmds->argv[0]));
-        return (NULL);
+		return (NULL);
 	}
 	path = my_little_getenv(shell->env_list, "PATH");
 	if (!path)
-        return (NULL);
-    path = check_the_path(path, shell->cmds->argv[0]);
-    if (path && (is_path_okey(path) == 0))
-        return (path);
-    free(path);
+		return (NULL);
+	path = check_the_path(path, shell->cmds->argv[0]);
+	if (path && (is_path_okey(path) == 0))
+		return (path);
+	free(path);
 	return (NULL);
+}
+
+void	works_ctrl_c(int signal)
+{
+	(void)signal;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
 void	execute_external(t_shell *shell)
@@ -139,9 +148,9 @@ void	execute_external(t_shell *shell)
 	pid_t	pid;
 
 	how_died = 0;
-    path = find_command_path(shell);
-    if (!path)
-        return ;
+	path = find_command_path(shell);
+	if (!path)
+		return ;
 	pid = fork();
 	if (pid < 0)
 	{
@@ -150,15 +159,15 @@ void	execute_external(t_shell *shell)
 	}
 	if (pid == 0)
 	{
-        signal(SIGINT, SIG_DFL);
-        signal(SIGQUIT, SIG_DFL);
-        //redirler de hata varsa execve çalışmamalı, o yüzden redirler de hata var mı diye kontrol etmek lazım
-        if (apply_redir(shell->cmds->redirects, shell))
-        {
-            free(path);
-            exit(1);
-        }
-        update_env_nodes(shell);
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+		//redirler de hata varsa execve çalışmamalı, o yüzden redirler de hata var mı diye kontrol etmek lazım
+		if (apply_redir(shell->cmds->redirects, shell))
+		{
+			free(path);
+			exit(1);
+		}
+		update_env_nodes(shell);
 		if (execve(path, shell->cmds->argv, shell->env))
 		{
 			perror("execve fail");
@@ -167,18 +176,18 @@ void	execute_external(t_shell *shell)
 	}
 	else
 	{
-        free(path);
+		free(path);
 		if (wait(&how_died) == -1)
 		{
 			perror("wait fail");
 			exit(1);
 		}
-        signal(SIGINT, works_crtl_c);
-        signal(SIGQUIT, SIG_IGN);
-        if (WIFEXITED(how_died))
-		    shell->exit_status = WEXITSTATUS(how_died);
-	    else if (WIFSIGNALED(how_died))
-		    shell->exit_status = 128 + WTERMSIG(how_died);
+		signal(SIGINT, works_ctrl_c);
+		signal(SIGQUIT, SIG_IGN);
+		if (WIFEXITED(how_died))
+			shell->exit_value = WEXITSTATUS(how_died);
+		else if (WIFSIGNALED(how_died))
+			shell->exit_value = 128 + WTERMSIG(how_died);
 		 /*wait() kullanmazsak child process zombie process olarak öylece kalır. Arka planda
 		boş yere yer kaplayan processler olur. İşletim sisteminin de belirli sayıda child
 		process açma hakkı olduğu için ileride sıkıntı oluşturur.*/
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: zedurak <zedurak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 13:25:09 by zedurak           #+#    #+#             */
-/*   Updated: 2026/05/02 18:25:30 by zedurak          ###   ########.fr       */
+/*   Updated: 2026/05/16 13:58:30 by zedurak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,82 +14,82 @@
 
 t_cmd *start_parser(char *input, t_shell *shell)
 {
-    (void)input;
-    (void)shell;
-    return (NULL);
+	(void)input;
+	(void)shell;
+	return (NULL);
 }
 
 void start_execute(t_shell *shell)
 {
-    t_cmd *cmd;
+	t_cmd *cmd;
 
-    cmd = shell->cmds;
-    if (!cmd || !cmd->argv)
-        return ;
-    /*Burada yapmaya çalıştığım pipe kullanımına göre ayırmak ve kullanılmadığı takdirde builtin ya da external
-    olarak çalıştırılması, ek olarak içeride redirect işlemleri kontrolü de yapılmalıdır.*/
-    if (cmd->next != NULL) //birden fazla cmd varsa pipe var demektir
-        pipe_working(shell); //cmd listesini dolaşarak pipe işlemlerini yapar
-		//en son child içinde en son child in exit kodunu shell->exit_status a yazmak lazım
+	cmd = shell->cmds;
+	if (!cmd || !cmd->argv)
+		return ;
+	/*Burada yapmaya çalıştığım pipe kullanımına göre ayırmak ve kullanılmadığı takdirde builtin ya da external
+	olarak çalıştırılması, ek olarak içeride redirect işlemleri kontrolü de yapılmalıdır.*/
+	if (cmd->next != NULL) //birden fazla cmd varsa pipe var demektir
+		pipe_working(shell); //cmd listesini dolaşarak pipe işlemlerini yapar
+		//en son child içinde en son child in exit kodunu shell->exit_value a yazmak lazım
 		//Bu pipe'ta 3 komut var. Bash'in kuralı: en sondaki komutun exit kodu önemli. ls hata verse bile, wc -l başarılıysa $? = 0 olur.
-    else if (is_builtin(cmd->argv[0], shell)) //cmd tek ise builtin mi diye kontrol eder
-        execute_builtin(cmd->argv[0], shell, 0); //forklamadan built-in komutları çalıştırır. İçeride redirect işlemleri kontrolü yapmayı unutma
+	else if (is_builtin(cmd->argv[0], shell)) //cmd tek ise builtin mi diye kontrol eder
+		execute_builtin(cmd->argv[0], shell, 0); //forklamadan built-in komutları çalıştırır. İçeride redirect işlemleri kontrolü yapmayı unutma
 		//builtinler doğru ise 0 dönmeli hatalı ise 1 dönmeli
-    else
-        execute_external(shell); //fork lazımdıır,external komutları çalıştırır. İçeride redirect işlemleri kontrolü yapmayı unutma
-    //!!sondaki else bir şekilde azaltılabilir mi diye düşünüyorum, çünkü cmd tek ise ve builtin değilse zaten external komut oluyor, yani tek if ile de halledilebilir gibi geliyor bana.!!
-    //externallerin nasıl çalıştırıldığını anlayınca tekrar değerlendirilecektir bu durum.!!
+	else
+		execute_external(shell); //fork lazımdıır,external komutları çalıştırır. İçeride redirect işlemleri kontrolü yapmayı unutma
+	//!!sondaki else bir şekilde azaltılabilir mi diye düşünüyorum, çünkü cmd tek ise ve builtin değilse zaten external komut oluyor, yani tek if ile de halledilebilir gibi geliyor bana.!!
+	//externallerin nasıl çalıştırıldığını anlayınca tekrar değerlendirilecektir bu durum.!!
 	//execute_external içinde waitpid'den gelen kodu
-    //shell->exit_status'a yazman lazım
+	//shell->exit_value'a yazman lazım
 }
 
 void lets_start_shell(t_shell *shell)
 {
-    while (1)
-    {
-        shell->input = readline("minishell> ");
-        if(shell->input == NULL) //crtl+d de readline NULL döner
-        {
-            write(2, "exit\n", 5);
-            rl_clear_history();
-            break ;
-        }
-        if (*shell->input != '\0') //bash de enter a basınca boş inputu kaydetmiyor soo biz de kaydetmiyoruz
-            add_history(shell->input);
-        shell->cmds = start_parser(shell->input, shell); 
-        if (shell->cmds)
-            start_execute(shell);
-        free(shell->input);
-    }
+	while (1)
+	{
+		shell->input = readline("minishell> ");
+		if(shell->input == NULL) //crtl+d de readline NULL döner
+		{
+			write(2, "exit\n", 5);
+			rl_clear_history();
+			break ;
+		}
+		if (*shell->input != '\0') //bash de enter a basınca boş inputu kaydetmiyor soo biz de kaydetmiyoruz
+			add_history(shell->input);
+		shell->cmds = start_parser(shell->input, shell); 
+		if (shell->cmds)
+			start_execute(shell);
+		free(shell->input);
+	}
 }
 
-void works_crtl_c(int signal)
+void works_ctrl_c(int signal)
 {
-    (void)signal;
-    write(1, "\n", 1);
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
+	(void)signal;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
 int main(int argc, char **argv, char **envp)
 {
-    t_shell *shell;
-    (void)argc;
-    (void)argv;
+	t_shell *shell;
+	(void)argc;
+	(void)argv;
 
-    signal(SIGINT, works_crtl_c);
-    signal(SIGQUIT, SIG_IGN);
-    shell = malloc(sizeof(t_shell));
-    if (!shell)
-        return (1); 
-    if (!init_shell(shell, envp)) //default değerlerle dolduruldu
-    {
-        write(2, "Initialization failed\n", 22);
-        free(shell);
-        return (1);
-    }
-    lets_start_shell(shell); //repl döngüsü başlatıldı
-    free(shell);
-    return (0);
+	signal(SIGINT, works_ctrl_c);
+	signal(SIGQUIT, SIG_IGN);
+	shell = malloc(sizeof(t_shell));
+	if (!shell)
+		return (1); 
+	if (!init_shell(shell, envp)) //default değerlerle dolduruldu
+	{
+		write(2, "Initialization failed\n", 22);
+		free(shell);
+		return (1);
+	}
+	lets_start_shell(shell); //repl döngüsü başlatıldı
+	free(shell);
+	return (0);
 }
