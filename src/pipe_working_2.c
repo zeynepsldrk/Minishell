f0d7230de7e088385bb09ff164cff00b68cc2b68
Zeynep Sila Durak <zedurak@k1m17s02.42istanbul.com.tr>
2026-06-06 21:12:21 +0300

    refactor: extract fd connection logic from pipe_working_2.c

--- src/pipe_working_2.c
+++ src/pipe_working_2.c
@@ -6,61 +6,28 @@
 /*   By: zedurak <zedurak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/02 14:50:44 by zedurak           #+#    #+#             */
-/*   Updated: 2026/05/16 13:08:33 by zedurak          ###   ########.fr       */
+/*   Updated: 2026/06/06 18:35:50 by zedurak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void execute_command(t_shell *shell)
+void	execute_command(t_shell *shell)
 {
-    if (!shell->cmds->argv || !shell->cmds->argv[0])
-    {
-        exit(1);
-    }
+	if (!shell->cmds->argv || !shell->cmds->argv[0])
+	{
+		exit(1);
+	}
 	if (is_builtin(shell->cmds->argv[0], shell))
-    {
+	{
 		execute_builtin(shell->cmds->argv[0], shell, 0, 1); //pipe içinde çalışacaksa in_pipe 1 olacak
-        exit(shell->exit_value);
-    }
+		exit(shell->exit_value);
+	}
 	else
 		execute_external(shell, 1); //child içinde ise 1 olacak
 }
 
-static void dup2_with_check(int i, int newfd, int **fd, int pipe_count)
-{
-	int fd_to_use;
-
-	if (newfd == STDOUT_FILENO)
-		fd_to_use = fd[i][1];
-	else
-		fd_to_use = fd[i][0];
-	if (ft_safe_dup2(fd_to_use, newfd) == -1)
-	{
-		ft_free_pipes(fd, pipe_count);
-		exit(1);
-	}
-}
-
-void connect_child_fds(int i, int cmd_count, int **fd)
-{
-    if ((cmd_count - 1) == 0)
-    {
-        return;
-    }
-	if (i == 0) //ilk komut
-        dup2_with_check(i, STDOUT_FILENO, fd, cmd_count - 1); //stdout'u pipe'ın yazma ucuna yönlendir
-	else if (i == cmd_count - 1) //son komut
-        dup2_with_check(i - 1, STDIN_FILENO, fd, cmd_count - 1); //stdin'i önceki pipe'ın okuma ucuna yönlendir
-	else //ortadaki komutlar
-	{
-        dup2_with_check(i - 1, STDIN_FILENO, fd, cmd_count - 1); //stdin'i önceki pipe'ın okuma ucuna yönlendir
-		dup2_with_check(i, STDOUT_FILENO, fd, cmd_count - 1); //stdout'u pipe'ın yazma ucuna yönlendir
-	}
-	ft_free_pipes(fd, cmd_count - 1); //child process tüm pipe'ları kapatır çünkü artık yönlendirme yapılmıştır
-}
-
-void execute_child_logic(t_shell *shell, t_cmd *cmd, int i)
+void	execute_child_logic(t_shell *shell, t_cmd *cmd, int i)
 {
 	shell->cmds = cmd;
 	connect_child_fds(i, shell->pipes.command_count, shell->pipes.fd);
@@ -74,9 +41,9 @@ void execute_child_logic(t_shell *shell, t_cmd *cmd, int i)
 	exit(127); //execve başarısızsa buradayız
 }
 
-void spawn_commands(t_shell *shell, pid_t *pid, int i)
+void	spawn_commands(t_shell *shell, pid_t *pid, int i)
 {
-	t_cmd *cmd;
+	t_cmd	*cmd;
 
 	cmd = shell->cmds;
 	while (shell->pipes.command_count > i) //komut sayısı - 1 kadar pipe oluştur
