f1ec8eace38ab5880f84d874ae7df580413cb394
Aleyna Say <sayaleyna27@gmail.com>
2026-06-07 21:43:02 +0300

    refactor: combine common env_value functions after merge

--- lexer/expand_utils.c
+++ lexer/expand_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expand_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asay <asay@student.42istanbul.com.tr>      +#+  +:+       +#+        */
+/*   By: marvin <asay@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/02 15:24:47 by asay              #+#    #+#             */
-/*   Updated: 2026/06/06 19:29:44 by asay             ###   ########.fr       */
+/*   Updated: 2026/06/07 21:24:20 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,37 +29,17 @@ char *get_env_key(char *str, int i)
         j++; 
     }
     key[j] = '\0';
-     printf("DEBUG get_env_key -> str: '%s', i_start: %d, key: '%s'\n", str, i - j, key); // geçici
     return (key);
 }
 
 char *get_env_value(t_shell *sh, char *key)
 {
-    int i;
-    int j;
-    int k;
     char *value;
 
-    i = 0;
-    j = ft_strlen(key) + 1;
-    k = 0;
-    value = NULL;
-    while(sh->env[i])
-    {
-        if (ft_strncmp(sh->env[i], key, ft_strlen(key)) == 0 && sh->env[i][ft_strlen(key)] == '=')
-        {
-            value = malloc(ft_strlen(sh->env[i]) + 1);
-            if(!value)
-                return NULL;
-            while(sh->env[i][j] != '\0')
-                value[k++] = sh->env[i][j++];
-            break ; //kopyalma sonrasi env icinde gezmeyi birakiyroum.
-        }
-        i++;
-    }
-    if(value)
-        value[k] = '\0'; //cagirdigin yerde "eger null döndüyse" kontrolü yapmayı unutma!!
-    return (value);
+    value = my_little_getenv(sh->env_list, key);
+    if (!value)
+        return (NULL);
+    return (ft_strdup(value));
 }
 
 char *ch_value(t_expander *exp)
@@ -72,7 +52,6 @@ char *ch_value(t_expander *exp)
     i = 0;
     j = 0;
     k = 0;
-    printf("str: '%s', key: '%s'\n", exp->str, exp->key);
     if(exp->value)
         new_context = malloc(ft_strlen(exp->str) + ft_strlen(exp->value) + 1);
     else
